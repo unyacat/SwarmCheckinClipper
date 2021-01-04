9dcec909610d2c3f3dd231ba13f1e9118f58eb01
unyacat <admin@unyacat.net>
2021-01-04 17:31:14 +0900

    Add: Home.vue content

--- src/Home.vue
+++ src/Home.vue
@@ -1,15 +1,45 @@
 <template>
-  <v-btn
-      elevation="3"
-  ></v-btn>
+  <v-content class="bg">
+    <v-container>
+      <v-row justify="center" align-content="center" style="height: 450px">
+        <v-spacer />
+        <v-col cols="12">
+          <div class="text-h3 text-sm-h3 text-md-h3 text-lg-h2 text-xl-h2 text-center">
+            Swarm Mapper
+          </div>
+        </v-col>
+        <v-col cols="12">
+          <div class="text-h5 text-center">
+            Swarm のチェックインを地図にプロットするだけ
+          </div>
+        </v-col>
+
+        <v-col cols="12">
+          <div class="text-center">
+            <v-btn elevation="2" color="orange" class="white--text">
+              Authenticate with Foursquare
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
 export default {
-name: "login"
+  name: "Home"
 }
 </script>
 
 <style scoped>
-
+  .bg {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-size: cover;
+    background-image: url("");
+  }
 </style>
