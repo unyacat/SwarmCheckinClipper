f8f55e8dd004d9d090548be7611514830343f7ad
unyacat <admin@unyacat.net>
2021-01-21 18:32:18 +0900

    Venue map

--- src/components/VenueDetails.vue
+++ src/components/VenueDetails.vue
@@ -1,6 +1,5 @@
 <template>
-  <v-app id="inspire">
-
+  <div>
     <v-app-bar
         app
         color="#ffa633"
@@ -11,59 +10,66 @@
     </v-app-bar>
 
 
-    <v-main>
-      <v-container>
-        <v-layout>
-          <v-row>
-            <v-col cols="12">
-              <p class="text-h3">
-                {{ this.name }}
-              </p>
-              <p class="text-h5">
-                {{ this.category }}
-              </p>
-            </v-col>
-          </v-row>
+    <v-container>
+      <div style="height: 300px; width: 100%">
+        <VenueMap :latlng="latlng"/>
+      </div>
+      <v-layout>
+        <v-row>
+          <v-col cols="12">
+            <p class="text-h3">
+              {{ name }}
+            </p>
+            <p class="text-h5">
+              {{ category }}
+            </p>
+          </v-col>
+        </v-row>
+      </v-layout>
 
-        </v-layout>
-        <v-simple-table>
-          <thead>
-          <tr>
-            <th class="text-left">
-              チェックイン日時
-            </th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr v-for="checkin in checkins" :key="checkin.cId">
-            <td> {{ unixToRealtime(checkin.cCreatedAt) }}</td>
-          </tr>
-          </tbody>
-        </v-simple-table>
-      </v-container>
-    </v-main>
-  </v-app>
+      <v-simple-table>
+        <thead>
+        <tr>
+          <th class="text-left">
+            チェックイン日時
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="checkin in checkins" :key="checkin.cId">
+          <td> {{ unixToRealtime(checkin.cCreatedAt) }}</td>
+        </tr>
+        </tbody>
+      </v-simple-table>
+    </v-container>
+  </div>
 </template>
 
 <script>
 // import AnimatedNumber from "./AnimatedNumber";
 import dayjs from "dayjs";
+import VenueMap from "@/components/VenueMap";
+
 export default {
   name: "result",
+  components: {VenueMap},
   // components: {AnimatedNumber},
   props: ['venueId'],
   data() {
     return {
       name: "",
       category: "",
+      latlng: [135, 35],
       checkins: []
     }
   },
   mounted() {
     this.$axios.get(process.env.VUE_APP_HOST + "/api/venue/" + this.venueId, {withCredentials: true}).then((res) => {
       this.checkins = res.data
+      this.latlng = [res.data[0].vLat, res.data[0].vLng]
       this.name = res.data[0].vName
-      this.category = res.data[0].vCategoryName
+      this.category = res.data[0].vCategoryNam
+      console.log(this.latlng)
     })
   },
   methods: {
---
+++ src/components/VenueMap.vue
@@ -0,0 +1,59 @@
+<template>
+  <l-map
+      :zoom="zoom"
+      :center="latlng"
+      :options="options"
+  >
+    <l-tile-layer
+        :visible="true"
+        url="	https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="Data <a href='https://www.openstreetmap.org/copyright'>© OpenStreetMap contributors</a>"
+        layer-type="base"
+    ></l-tile-layer>
+    <l-circle
+        :lat-lng="latlng"
+        :radius="circle.radius"
+        :color="circle.color"
+        :fillColor="circle.fillColor"
+        :fillOpacity="circle.fillOpacity"
+    />
+  </l-map>
+</template>
+
+<script>
+import {LMap, LTileLayer, LCircle} from "vue2-leaflet";
+
+export default {
+  name: "VenueMap",
+  components: {
+    LMap,
+    LTileLayer,
+    LCircle
+  },
+  props: ['latlng'],
+  mounted() {
+    console.log(this.latlng)
+  },
+  data() {
+    return {
+      zoom: 15,
+      circle: {
+        radius: 30,
+        color: "orange",
+        fillColor: "orange",
+        fillOpacity: 1
+      },
+      options: {
+        useCache: true,
+        zoomControl: false,
+        crossOrigin: true,
+        preferCanvas: true,
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
