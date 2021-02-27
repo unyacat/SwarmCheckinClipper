2372d4626a20c20016ae3d8a014b59ab822ed5f3
unyacat <admin@unyacat.net>
2021-02-27 12:43:24 +0900

    sendjwt

---
+++ src/components/LoadCheckins.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-btn class="ma-2" color="primary" dark @click="load"> 読み込む </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "load-checkins",
+  created: function () {
+    if (localStorage.getItem("access_token") === null) {
+      this.$router.push("/");
+    }
+  },
+  methods: {
+    load() {
+      this.$axios
+        .get(process.env.VUE_APP_HOST + "/api/load_checkins", {
+          headers: {
+            Authorization: "Bearer " + localStorage.access_token,
+          },
+        })
+        .then(this.$router.push("result"));
+    },
+  },
+};
+</script>
+
+<style></style>
